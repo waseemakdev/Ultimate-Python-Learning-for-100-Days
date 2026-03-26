a2b6f9688d2e86b065aeda65c8398990a88a292a
Waseem Akram <74070709+waseemakdev@users.noreply.github.com>
2026-03-26 19:09:29 +0500

    Basic Code

---
+++ Chapter1/Data Types and Variables.py
@@ -0,0 +1,83 @@
+print("Hello World","first Program")
+print(23)
+print(23+54)
+
+Data="Waseem"
+print("Hello",Data)
+
+age=23
+print("My age is:", age)
+
+# Print the data type of variable
+print(type(age))
+print(type(Data))
+(age>20)
+
+# boolean
+True and False
+
+# Keywords
+
+# Python is case sensitive let suppose if we create a variable with capital A "Apple" and "apple"
+# both of these are not same variables.
+
+
+# Sum of 2 Numbers
+
+a = 4
+b = 5 
+sum=a+b
+print(sum)
+
+# Types of Operators
+
+# Arithmetic Operators
+#  +,-,*,/,%,
+# ** This operator is used for getting the power of variables e.g. a^b
+a=5
+b=3
+solution=a**b
+print(solution)
+
+# Relational Operators
+# ==, >=, <,>,<=,
+
+# Assignement Operator
+# The work of assignment operator is assign another value.
+# =, +=, -=, *=, /=, %=, **=
+
+# Logical Operator
+# not, and, or,
+
+print(not True)
+print(not False)
+
+# and operator
+val1=True
+val2=True
+print("Answer Operator",val1 and val2)
+
+# Type Conversion
+
+# Type Conversion mean in python it's automatically convert into their variable data type e.g.
+a=5 
+b=5.0
+sum1=a+b
+print(sum1) # it'll automatically print float details.
+
+# than we use type casting
+# Type casting is only work in number specific and String specific. 
+d= int('2')
+c=2
+s=d+c
+print(s)
+
+
+# Input in Python.
+# first_Number=int(input("Enter Number"))
+# second_Number=int(input("Enter 2nd number"))
+# print("Sum of Numbers:", first_Number + second_Number3)
+
+first_Number=int(input("Enter Number"))
+
+print("Area of a Square:", first_Number* first_Number )
