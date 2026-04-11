a4ba84957f73c75b1b734366b8989a08618c93e4
Waseem Akram <74070709+waseemakdev@users.noreply.github.com>
2026-04-11 08:43:10 +0500

    Strings update

---
+++ Chapter1/Strings lists and tuples.py
@@ -0,0 +1,96 @@
+str1='Data tyopes is this is the way tto use.'
+print(str1)
+
+# We describe strings creation in single quotes, double quouts, and third quotes.
+# Strings concatenate,
+# find the length of string 
+# print("Length of Str:", len(str1))
+
+
+# indexing in string: When ever a string create it'll directly assign the index in that memory.
+# print(str1[2])
+
+
+# Slicing
+# Slicing is term like we cut an apple in center and get the slicie so it's like this when we 
+# cut the data in string or we use in machine learning for data slicing or clearning.
+# print(str1[1:4]) print data from 1 index to 4 index in slice.
+# print(str1[1:])  print data fron start to end in slice.
+# print(str1[:4])   print the data to end in slice.
+# print(str1[1:4])
+
+# Negative index in slicing
+# print(str1[-5:-1]) it'll show the from end of string to last 5 index
+# print(str1[-12:])
+
+
+# Strings Functions.
+str2="i'm a coder and I write codes."
+# print(str2.endswith("s.")) this will check if the string is end with required characters.
+# print(str2.capitalize()) this will print the first letter of string capital if it's small.
+# print(str2.replace("o","a")) this will replace the words in strings.
+# print(str2.find("a")) this will find the specific word or number.
+strnum="1234543232222"
+# print(strnum.find("5")) this function will only work for strings not for integers.
+
+# print(str2.count("o")) this will count the exact count of that word.
+# print(str2.count("ode")) this will also count the exact same number.
+
+#some testing example:
+# name=input("Enter the name :")
+# print("Length of your name: ", len(name))
+
+# Conditional Statements:
+
+# age=21
+# if(age>18):
+#     print("apply for license.")
+
+#     number=int(input("Enter the number for checking :"))
+#     # rem = number % 2
+#     if(number % 2 ==0):
+#         print("Even nunber")
+#     else:
+#         print("Odd Number")
+
+# find greatest number.
+
+# n1= 3
+# n2=4
+# n3=1
+# if(n1>n2 and n1>n3):
+#     print("Greatest number: ",n1 )
+# elif(n2>n1 and n2>n3):
+#     print("Greatest number: ", n2)
+# else:
+#     print(n3)
+        
+        # find the mulitple of numbers;
+# num=int(input("enter number to find the mulitple: "))    
+# if(num%7==0):
+#     print("this is multiple of 7")    
+# else:
+#     print("This is not multiple of 7")
+
+
+
+
+# List & Tuples in python.
+
+# Lists how they work and their functions.
+
+# list1=[1,2,2,2,2,4]
+
+
+# Tuples::
+    # Tuples are like lists but they are immutable. we cannot change the value it's mean immutable.
+
+# tup=(3,4,3,5,4,4)
+# print(tup)
+
+tuples=(4,5,6,7,3,2,4)
+
+
+print(tuples)
+
+
