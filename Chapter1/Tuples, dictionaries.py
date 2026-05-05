f236b980061785770f69111811807c7893b7779b
Waseem Akram <74070709+waseemakdev@users.noreply.github.com>
2026-05-05 21:55:02 +0500

    Intial commit

--- Chapter1/Tuples, dictionaries.py
+++ Chapter1/Tuples, dictionaries.py
@@ -1,1 +1,7 @@
 
+## Tuples:
+## Tuples is collection of items like DB it work nearly same as in python as well.
+
+## Dictionaries:
+## Dictionary is a type of like map in python. it'll declare like this:
+tuples_list=(4,5,6,"Waseem","Data")
